a1b2cabd84a7509e69643fe8d255d89310c46cad
ryoasaka12345 <ryo.asaka@g.pascalia.co.jp>
2021-05-28 11:31:38 +0900

    new file:   assets/css/index.css 	new file:   assets/js/index.js 	new file:   index.php 	new file:   modules/home.php 	new file:   modules/partials/footer.php 	new file:   modules/partials/header.php 	new file:   modules/partials/sidebar.php

---
+++ assets/js/index.js
@@ -0,0 +1,53 @@
+/* 
+
+    scroll the page to the top
+
+*/
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+/* 
+
+    hide when the page already at the top
+
+*/
+window.addEventListener('scroll', function () {
+    const currentScrollPosition = window.scrollY;
+    // scrollY returns the number of pixels that the document is currently scrolled vertically
+
+    const scrollBtn = document.getElementById('scroll');
+    // in css, you query the html tag is: #scroll
+
+    if (currentScrollPosition !== 0) {
+        scrollBtn.setAttribute('style', 'display: block;');
+        // this will add the style attribute
+        // <a style="display: block;">...</a>
+    } else {
+        scrollBtn.setAttribute('style', 'display: none;');
+    }
+});
+
+/* 
+
+    check the current position of the page and hide "scrollBtn"
+
+*/
+function toggleScrollButton() {
+    const lastPosition = window.scrollY;
+    const scrollBtn = document.getElementById('scroll');
+
+    if (lastPosition !== 0) {
+        scrollBtn.setAttribute('style', 'display: block;');
+    } else {
+        scrollBtn.setAttribute('style', 'display: none;');
+    }
+}
+
+window.addEventListener('scroll', function () {
+    toggleScrollButton();
+});
+
+window.addEventListener('load', () => {
+    toggleScrollButton();
+});
